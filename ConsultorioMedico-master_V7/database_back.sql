9b811fcfb7f45dcd39e47a34d54e43b43792fe86
DanielAlexanderBeltran <danbelven@gmail.com>
2018-06-05 10:53:25 -0500

    solo base cambiada

--- ConsultorioMedico-master_V7/database_back.sql
+++ ConsultorioMedico-master_V7/database_back.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `clinica` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `clinica`;
 -- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
 --
 -- Host: localhost    Database: clinica
@@ -1719,4 +1721,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-05 10:42:30
+-- Dump completed on 2018-06-05 10:52:33
